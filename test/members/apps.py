88da6ce788dbd153b0e7f3e464ca5a3464e72d3d
David Jacques Vandenbrouck <dvandenb@c1r14s5.42lausanne.ch>
2024-03-08 17:18:27 +0100

    Undo merge

--- test/members/apps.py
+++ test/members/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MonappConfig(AppConfig):
+class MembersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'monapp'
+    name = 'members'
---
+++ test/test.py
@@ -0,0 +1,8 @@
+from time import sleep
+
+def p(a, b, c):
+	sleep(1)
+	print (str(a) + " " + str(b) + " " + str(c))
+
+
+p(12, "Hello", 13)
