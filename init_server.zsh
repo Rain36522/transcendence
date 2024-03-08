5e41ae6eec72dde4ac1783e395df7e34150c7133
David Jacques Vandenbrouck <dvandenb@c1r14s5.42lausanne.ch>
2024-03-08 14:58:04 +0100

    Created test models, dockerfile

---
+++ init_server.zsh
@@ -0,0 +1,3 @@
+source ./test/bin/activate
+python3 ./test/manage.py migrate
+python3 ./test/manage.py runserver
--- test/test/settings.py
+++ test/test/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,9 +76,13 @@ WSGI_APPLICATION = 'test.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "example",
+        "HOST": "localhost",
+        "PORT": "5432", 
     }
 }
 
