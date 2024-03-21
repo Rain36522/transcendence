92310697543d99e4e69316b67d74453bd21ad4e3
David Vandenbrouck <39681796+DavePie@users.noreply.github.com>
2024-03-21 09:08:19 -0400

    Added websocket for django - game server (#55)

--- TranServer/TranServer/routing.py
+++ TranServer/TranServer/routing.py
@@ -1,6 +1,8 @@
 from django.urls import re_path
 from chat.consumer import ChatConsumer
+from game.consumer import GameServerConsumer
 
 websocket_urlpatterns = [
     re_path(r'ws/your_url/$', ChatConsumer.as_asgi()),
+    re_path(r'wsgameserver/$', GameServerConsumer.as_asgi())
 ]
