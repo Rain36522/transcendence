4a85aa70a84486a95f6f620d830822fe109d9059
cduffaut <119802570+cduffaut@users.noreply.github.com>
2024-03-26 14:57:34 +0100

    Added html pages and integrated with Django (#58)
    Co-authored-by: cduffaut <cecileduffaut@gmail.com>

--- TranServer/chat/urls.py
+++ TranServer/chat/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path
 from .views import ChatListView, chat_view, MessageListView, ws_view
 from .consumer import ChatConsumer
+# cecile import
+from .views import general_chat
+#
 
 urlpatterns = [
      path('chats/', chat_view, name='chats'),
      path('api/chat/', ChatListView.as_view(), name='chat_api'),
      path('api/chat/<str:chat_id>/', ChatListView.as_view(), name='chat_api'),
      path('api/messages/<str:chat_id>/', MessageListView.as_view(), name='chat_message_view'),
-     path('test/', ws_view, name='ws_view')
+     path('test/', ws_view, name='ws_view'),
+	 # cecile path
+	 path('generalchat/', general_chat, name='general_chat')
+	 #
 ]
 
 websocket_urlpatterns = [
