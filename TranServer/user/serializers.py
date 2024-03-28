3283003c1ad085afbd19547d40515a9d3e50faeb
David Vandenbrouck <39681796+DavePie@users.noreply.github.com>
2024-03-28 07:38:01 -0400

    added signin, log out, log in (#66)

--- TranServer/user/serializers.py
+++ TranServer/user/serializers.py
@@ -1,7 +1,23 @@
 from rest_framework import serializers
 from .models import User
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer_Username(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username']
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'email']
+        extra_kwargs = {
+            'password': {'write_only': True, 'required': True},
+            'username': {'required': True},
+            'email': {'required': True},
+        }
+    def create(self, validated_data):
+        password = validated_data.pop('password')
+        user = User.objects.create(**validated_data)
+        user.set_password(password)  # Hash the password
+        user.save()
+        return user
--- TranServer/user/urls.py
+++ TranServer/user/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from .views import SignUpView
-from .views import profile, invite_user
-from .views import dashboard
-from .views import profil_utilisateur
-from .views import account_information, user_dashboard, user_login, user_register, social_management, user_login_api
+
+from .views import account_information, user_dashboard, user_login, user_register, social_management, user_login_api, api_signup, logout_view
 from django.views.generic.base import RedirectView
 
 
@@ -15,14 +13,9 @@ urlpatterns = [
     path ('login/', user_login, name='user_login'),
     path ('register/', user_register, name='user_register'),
     path ('socialManagement/', social_management, name='social_management'),
-
-    path('', RedirectView.as_view(url='login', permanent=True)),
-    path('api/login/', user_login_api.as_view(), name='login_api')
-    # path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    # path('signup/', SignUpView.as_view(), name='signup'),
-    # path('profile/', profile, name='profile'),
-    # path('profile/dashboard/', dashboard, name='dashboard'),
-    # path('profile/<str:username>/', views.profil_utilisateur, name='profile'),
-    # path('invite/', invite_user, name='invite'),
+    path('', RedirectView.as_view(url='dashboard/', permanent=True)),
+    path('api/login/', user_login_api, name='login_api'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('api/signup/', api_signup, name='api_signup'),
+    path('logout/', logout_view, name='user_logout')
 ]
