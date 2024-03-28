c974ceff6cb9198476cb704ba6f540973453a91f
David Jacques Vandenbrouck <dvandenb@c1r14s5.42lausanne.ch>
2024-03-28 12:34:36 +0100

    added signin, log out, log in

--- TranServer/user/views.py
+++ TranServer/user/views.py
@@ -1,8 +1,10 @@
-from django.contrib.auth import authenticate, login, get_user_model
+from django.contrib.auth import authenticate, login, get_user_model, logout
 from django.http import HttpResponse
-from django.template import RequestContext
 from django.urls import reverse_lazy
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.response import Response
+from rest_framework.renderers import JSONRenderer
 
 from django.views import generic
 from .forms import CustomUserCreationForm, InvitationForm, AcceptInviteForm
@@ -11,6 +13,18 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import User
 from django.contrib import messages
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .serializers import UserSerializer
+
+@csrf_exempt
+@api_view(['POST'])
+@renderer_classes([JSONRenderer])
+def api_signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'message': 'User created successfully'})
+    return Response(serializer.errors, status=400)
 
 @login_required
 def invite_user(request):
@@ -44,29 +58,36 @@ def invite_user(request):
 
     return render(request, 'invite_user.html', {'invite_form': invite_form, 'accept_form': accept_form})
 
-class user_login_api(APIView):
-    def post(self, request):
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-            login(request, user)
-            return redirect('home')  # Redirige vers la page d'accueil après connexion
-        else:
-            return JsonResponse({'error': 'Invalid username or password'}, status=400)
+@csrf_exempt
+@api_view(['POST'])
+@renderer_classes([JSONRenderer])
+def user_login_api(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    user = authenticate(request, username=username, password=password)
+    if user is not None:
+        login(request, user)
+        return redirect('user_dashboard') 
+    else:
+        return JsonResponse({'error': 'Invalid username or password'}, status=400)
 
 class SignUpView(generic.CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'html/register.html'
 
 @login_required
 def account_information(request):
     return render(request, 'html/accountInformation.html')
 
+def logout_view(request):
+    logout(request)
+    return redirect('user_login')
+
+
 @login_required
 def user_dashboard(request):
-    return render(request, 'html/dashboard.html')
+    return render(request, 'html/dashboard.html', {'user': request.user})
 
 def user_login(request):
     return render(request, 'html/login.html')
