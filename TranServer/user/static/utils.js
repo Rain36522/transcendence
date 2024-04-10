0e01b3d8bd1ecbcae59f494c26b6518688f3d6e4
David Vandenbrouck <39681796+DavePie@users.noreply.github.com>
2024-04-10 18:02:56 +0200

    Delete files, add special loading script to html (#99)
    Co-authored-by: David Jacques Vandenbrouck <dvandenb@c1r14s5.42lausanne.ch>

--- TranServer/user/static/utils.js
+++ TranServer/user/static/utils.js
@@ -1,9 +1,43 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener('click', function (event) {
+        const target = event.target;
+        if (target.tagName === 'A' && target.getAttribute('href') !== '#') {
+            event.preventDefault();
+            const url = target.getAttribute('href');
+            fetchPage(url);
+            history.pushState(null, null, url);
+        }
+    });
+
+    async function fetchPage(url) {
+        try {
+            const response = await fetch(url);
+            const html = await response.text();
+            replaceContent(html);
+        } catch (error) {
+            console.error('Error fetching page:', error);
+        }
+    }
+
+    function replaceContent(html) {
+        document.body.innerHTML = html;
+    }
+
+    window.onpopstate = function () {
+        const url = window.location.pathname;
+        fetchPage(url);
+    };
+});
+
+
 function getCookie(name) {
     const cookieValue = document.cookie.split(';')
-	.find(cookie => cookie.trim().startsWith(name + '='));
+        .find(cookie => cookie.trim().startsWith(name + '='));
     if (cookieValue) {
         return cookieValue.split('=')[1];
     } else {
         return null;
     }
 }
+
+
