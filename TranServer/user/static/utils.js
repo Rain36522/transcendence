339429b5cb90173db87f38ffa7dd176a2f082fcf
David Jacques Vandenbrouck <dvandenb@c1r14s5.42lausanne.ch>
2024-04-10 18:28:38 +0200

    Load scripts dynamically

--- TranServer/user/static/utils.js
+++ TranServer/user/static/utils.js
@@ -14,9 +14,33 @@ document.addEventListener("DOMContentLoaded", function () {
             const response = await fetch(url);
             const html = await response.text();
             replaceContent(html);
+            loadScriptsFromHTML(html);
+
         } catch (error) {
             console.error('Error fetching page:', error);
         }
+    }
+
+    async function loadScriptsFromHTML(html) {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const scriptTags = doc.querySelectorAll('script');
+
+        const scriptUrls = Array.from(scriptTags)
+            .map(scriptTag => scriptTag.getAttribute('src'))
+            .filter(src => src);
+
+        await Promise.all(scriptUrls.map(loadScript));
+    }
+
+    function loadScript(url) {
+        return new Promise((resolve, reject) => {
+            const script = document.createElement('script');
+            script.src = url;
+            script.onload = resolve;
+            script.onerror = reject;
+            document.head.appendChild(script);
+        });
     }
 
     function replaceContent(html) {
