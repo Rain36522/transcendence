d2550c8f3e3d7b880d420ad8f53ce9133f6927ba
Cécile Duffaut <cecileduffaut21@gmail.com>
2024-04-13 17:03:15 +0200

    Generak chat triplons deleted looool

--- TranServer/chat/static/generalchat2.js
+++ TranServer/chat/static/generalchat2.js
@@ -113,17 +113,6 @@ document.querySelectorAll('.invite-button').forEach(button => {
     button.addEventListener('click', handleInviteButtonClick);
 });
 
-
-// Attach the event listener directly to each invite-button
-document.querySelectorAll('.invite-button').forEach(button => {
-    button.addEventListener('click', handleInviteButtonClick);
-});
-
-// Attach the event listener directly to each invite-button
-document.querySelectorAll('.invite-button').forEach(button => {
-	button.addEventListener('click', handleInviteButtonClick);
-});
-
 // Gestion du clic sur le fond flou pour fermer la popup
 blurBackground.addEventListener('click', function () {
 	closePopup();
